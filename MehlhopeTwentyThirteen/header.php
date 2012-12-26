2dc3234235845e1c4036c405e5570c8eeb9607ae
Matt Mehlhope <mmehlhope@gmail.com>
2012-12-26 13:20:33 -0500

    Updating header, styles, media query polyfill, and icon

--- MehlhopeTwentyThirteen/header.php
+++ MehlhopeTwentyThirteen/header.php
@@ -9,7 +9,7 @@ Template Name: Header
         <nav class="clearfix" role="navigation">
             <ul class="navItems" role="list">
                 <li role="listitem">
-                    <a href="https://dl.dropbox.com/s/9xsirjpjrzwmore/Matt%20Mehlhope%20Resume%20-%20NA.pdf?dl=1" class="btn" title="Matt Mehlhope's Resume">My Resume (PDF)</a>
+                    <a href="https://dl.dropbox.com/s/9xsirjpjrzwmore/Matt%20Mehlhope%20Resume%20-%20NA.pdf?dl=1" class="btn" title="Matt Mehlhope's Resume"><span class="text">My Resume (PDF)</span><img class="icon" src="<?php bloginfo('template_url'); ?>/images/icon-pdf-white.png" height="25px" alt="Download Matt Mehlhope's Resume"></a>
                 </li>
             </ul>
         </nav>
